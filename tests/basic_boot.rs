d3db0c2cbb04ca227fd2adb642d070b3868f2b27
rac-sri <rac.sri25@gmail.com>
2024-01-12 19:51:18 +0530

    interuppts

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,4 +1,3 @@
-
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
@@ -9,25 +8,22 @@ use core::panic::PanicInfo;
 use blog_os::println;
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
+pub extern "C" fn _start() -> !{
     test_main();
-
-    loop {}
+    loop{}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]){
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
 }
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
